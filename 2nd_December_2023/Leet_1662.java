24dfd5121101df23e8200ea8708429ff0b83b71b
Fiza32 <mdfaizan878@gmail.com>
2023-12-02 11:58:07 +0530

    Check If Two String Arrays are Equivalent

--- 2nd_December_2023/Leet_1662.java
+++ 2nd_December_2023/Leet_1662.java
@@ -23,3 +23,16 @@ public class Leet_1662 {
         return str1.equals(str2);
     }
 }
+
+
+/*
+ *  Time Complexity:
+    Concatenation: O(n) due to iterating through each word in both arrays.
+    Comparison: O(n) due to comparing the length of the final strings.
+    Overall: O(n)
+
+    Space Complexity:
+    Concatenation: O(n) due to storing the concatenated strings in StringBuilder objects.
+    Comparison: O(1) as temporary variables have constant memory usage.
+    Overall: O(n)
+ */
