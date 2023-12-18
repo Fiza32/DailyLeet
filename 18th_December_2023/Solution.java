4cfff3df102bb5759f780e7a9e80ac310ce845ec
Fiza32 <mdfaizan878@gmail.com>
2023-12-18 23:51:15 +0530

    1913. Maximum Product Difference between Two Pairs

---
+++ 18th_December_2023/Solution.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+
+class Solution {
+    public int maxProductDifference(int[] nums) {
+        int n = nums.length;
+
+        Arrays.sort(nums);
+
+        int pro1 = nums[0] * nums[1];
+        int pro2 = nums[n-2] * nums[n-1];
+
+        return pro2 - pro1;
+    }
+}
+
+class Solution2 {
+    public int maxProductDifference(int[] nums) {
+        int n = nums.length;
+
+        int fstMax = Integer.MIN_VALUE;;
+        int secMax = Integer.MIN_VALUE;
+
+        // Find Max & secMax element
+        for(int num : nums){
+            if(num > fstMax){
+                secMax = fstMax;
+                fstMax = num;
+            }
+
+            else if(num <= fstMax && num > secMax){
+                secMax = num;
+            }
+        }
+
+        int fstMin = Integer.MAX_VALUE;
+        int secMin = Integer.MAX_VALUE;
+        
+        // Find Min & secMin element
+        for(int num : nums){
+            if(num < fstMin){
+                secMin = fstMin;
+                fstMin = num;
+            } 
+            
+            else if(num >= fstMin && num < secMin){
+                secMin = num;
+            }
+        }
+
+        return (fstMax * secMax) - (fstMin * secMin);
+    }
+}
