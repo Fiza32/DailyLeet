585d667ddfae5e4c878e0995f8750aadb05aa0d9
Fiza32 <mdfaizan878@gmail.com>
2023-12-24 23:36:00 +0530

    1662. Check If Two String Arrays are Equivalent

--- 1st_December_2023/Leet_1662.java
+++ 1st_December_2023/Leet_1662.java
@@ -84,3 +84,47 @@ class SolutionThird {
         return word1.length == i && word2.length == j;
     }
 }
+
+/*
+ *  **Here's the intuition behind the code:**
+
+**1. Purpose:**
+
+- The code is designed to determine whether two arrays of strings, `word1` and `word2`, contain the same sequence of characters when combined.
+- It essentially checks if they represent the same string, even though they might be broken into different words within the arrays.
+
+**2. Approach:**
+
+- It uses two sets of indices to track progress:
+    - `i` and `j` for iterating through the individual words within the arrays
+    - `x` and `y` for iterating through the characters within each word
+- It compares corresponding characters one by one, using `charAt()` to access them.
+- It handles word boundaries by resetting `x` and `y` to 0 and advancing `i` or `j` when the end of a word is reached.
+
+**3. Key Steps:**
+
+1. **Initialization:**
+   - Sets `i`, `j`, `x`, and `y` to 0 to start at the beginning of both arrays and their first words.
+
+2. **Character Comparison Loop:**
+   - Continues as long as there are more words and characters to compare:
+     - Compares characters at `word1[i].charAt(x)` and `word2[j].charAt(y)`.
+     - If they don't match, returns `false` immediately, indicating the arrays are not equal.
+     - Increments `x` and `y` to move to the next characters.
+     - If either `x` or `y` reaches the end of a word:
+       - Resets it to 0 to start the next word.
+       - Increments `i` or `j` to move to the next word in the corresponding array.
+
+3. **Final Check:**
+   - After the loop, checks if both arrays have been fully traversed:
+     - If `i` and `j` have reached the lengths of `word1` and `word2` respectively, it means all characters have matched, so returns `true`.
+     - Otherwise, returns `false` as there are still characters left to compare.
+
+**In simpler terms:**
+
+- The code imagines the two arrays as long strings, then carefully walks through them character by character, making sure each pair of characters matches.
+- It pays attention to word boundaries, ensuring it doesn't try to compare characters across different words.
+- If it finds any mismatch, it immediately stops and says the arrays are not equal.
+- If it manages to compare all characters without any mismatches, it confidently declares that the arrays are indeed equal.
+
+ */
